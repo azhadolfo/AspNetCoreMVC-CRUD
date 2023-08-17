3fa7ac25c72c57284805eb7087e6da48d796f736
Gray <azhadolfo1@gmail.com>
2023-08-17 09:45:58 +0800

    creating a project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudInASPNETMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudInASPNETMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
